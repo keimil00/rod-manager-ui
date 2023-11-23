a22c2fcc2591b161de1a0df05d1526f01f247d12
Jakub Roszkowski <jakubr1701@gmail.com>
2023-11-23 21:10:05 +0100

    quiq diagrams upgrades

--- src/app/features/votings/finished-votings/finished-votings.component.ts
+++ src/app/features/votings/finished-votings/finished-votings.component.ts
@@ -4,77 +4,81 @@ import {VotingItem, VotingItem2} from "../voting-item.model";
 import {VotingsService} from "../votings.service";
 
 @Component({
-  selector: 'app-finished-votings',
-  templateUrl: './finished-votings.component.html',
-  styleUrls: ['./finished-votings.component.scss']
+    selector: 'app-finished-votings',
+    templateUrl: './finished-votings.component.html',
+    styleUrls: ['./finished-votings.component.scss']
 })
 export class FinishedVotingsComponent implements AfterViewInit {
 
-  // @ts-ignore
-  finishedVotes: VotingItem2[]
-  // @ts-ignore
-  @ViewChildren('chart') chartElements: QueryList<ElementRef>;
+    // @ts-ignore
+    finishedVotes: VotingItem2[]
+    // @ts-ignore
+    @ViewChildren('chart') chartElements: QueryList<ElementRef>;
 
-  constructor(private votingService: VotingsService) {
-  }
+    constructor(private votingService: VotingsService) {
+    }
 
-  ngOnInit() {
-    this.getFinishedVotes();
-  }
+    ngOnInit() {
+        this.getFinishedVotes();
+    }
 
-  getFinishedVotes() {
-    this.votingService.getFinishedVotes().subscribe((votes: VotingItem2[]) => {
-      this.finishedVotes = votes;
-    });
-  }
+    getFinishedVotes() {
+        this.votingService.getFinishedVotes().subscribe((votes: VotingItem2[]) => {
+            this.finishedVotes = votes;
+        });
+    }
 
-  ngAfterViewInit() {
-    this.generateCharts();
-  }
+    ngAfterViewInit() {
+        this.generateCharts();
+    }
 
-  generateCharts() {
-    this.chartElements.forEach((element, index) => {
-      const vote = this.finishedVotes[index];
+    generateCharts() {
+        this.chartElements.forEach((element, index) => {
+            const vote = this.finishedVotes[index];
 
-      const chartOptions: ApexCharts.ApexOptions = {
-        theme: {
-          mode: 'light',
-          palette: 'palette4',
-        },
-        series: vote.options.map((option: { votes: any; }) => option.votes),
-        chart: {
-          type: 'donut',
-          width: 420
-        },
-        dataLabels: {
-          enabled: true
-        },
-        fill: {
-          type: "gradient"
-        },
-        labels: vote.options.map((option: { label: any; }) => option.label),
-        legend: {},
-        responsive: [
-          {
-            breakpoint: 600,
-            options: {
-              chart: {
-                type: 'pie',
-                width: '100%' // Ustawienie szerokości na 70% dla ekranów o szerokości poniżej 480px
-              },
-              legend: {
-                position: "bottom",
-                horizontalAlign: 'center',
-              },
-              dataLabels: {
-                enabled: true
-              },
-            }
-          },
-        ]
-      };
-      const chart = new ApexCharts(element.nativeElement, chartOptions);
-      chart.render();
-    });
-  }
+            const chartOptions: ApexCharts.ApexOptions = {
+                theme: {
+                    mode: 'light',
+                    palette: 'palette4',
+                },
+                series: vote.options.map((option: { votes: any; }) => option.votes),
+                chart: {
+                    type: 'donut',
+                    width: 550
+                },
+                dataLabels: {
+                    enabled: true
+                },
+                fill: {
+                    type: "gradient"
+                },
+                labels: vote.options.map((option: { label: any; }) => option.label),
+                legend: {
+                    position: "bottom",
+                    horizontalAlign: 'center',
+                    fontSize: '14px',
+                },
+                responsive: [
+                    {
+                        breakpoint: 600,
+                        options: {
+                            chart: {
+                                type: 'pie',
+                                width: '100%' // Ustawienie szerokości na 70% dla ekranów o szerokości poniżej 480px
+                            },
+                            legend: {
+                                position: "bottom",
+                                horizontalAlign: 'center',
+                            },
+                            dataLabels: {
+                                enabled: true
+                            },
+                        }
+                    },
+                ]
+            };
+            const chart = new ApexCharts(element.nativeElement, chartOptions);
+            chart.render();
+        });
+    }
 }
