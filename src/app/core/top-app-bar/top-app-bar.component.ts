e00382d2ae156a6f23f6da538bb3eb934fc8727f
Jakub Roszkowski <jakubr1701@gmail.com>
2023-11-24 10:15:47 +0100

    upgrade Top bar

--- src/app/core/top-app-bar/top-app-bar.component.ts
+++ src/app/core/top-app-bar/top-app-bar.component.ts
@@ -14,7 +14,14 @@ import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 })
 export class TopAppBarComponent {
   isInGardenInfoComponent: boolean = false;
+  isInHomeComponent: boolean = false;
+  isInCalendarComponent: boolean = false;
+  isInVotingComponent: boolean = false;
+  isInMyGardenPlotInfoComponent: boolean = false;
+  isInGardenOffers: boolean = false;
+
   @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
+
 
   constructor(private router: Router,
               private storageService: StorageService,
@@ -27,6 +34,11 @@ export class TopAppBarComponent {
       if (event instanceof NavigationEnd) {
         const urlSegments = this.router.url.split('/');
         this.isInGardenInfoComponent = urlSegments.includes('garden-info');
+        this.isInHomeComponent = urlSegments.includes('home');
+        this.isInMyGardenPlotInfoComponent = urlSegments.includes('my-garden-plot-info');
+        this.isInCalendarComponent = urlSegments.includes('calendar');
+        this.isInVotingComponent = urlSegments.includes('voting');
+        this.isInGardenOffers = urlSegments.includes('garden-offers');
       }
     });
     this.breakpointObserver.observe('(max-width: 1250px)').subscribe(result => {
